c264b184f56cadd2341d13ac98b5b66998b71413
Jamb000h <jamb000h@gmail.com>
2018-10-11 21:48:59 +0300

    PSQL

--- src/main/java/jonnekan/harjoitustyokaksi/Main.java
+++ src/main/java/jonnekan/harjoitustyokaksi/Main.java
@@ -22,32 +22,46 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // asetetaan portti jos heroku antaa PORT-ympäristömuuttujan
+        if (System.getenv("PORT") != null) {
+            Spark.port(Integer.valueOf(System.getenv("PORT")));
+        }
+        
         Spark.get("*", (req, res) -> {
  
-            List<String> huonekalut = new ArrayList<>();
-
-            // avaa yhteys tietokantaan
-            Connection conn
-                    = DriverManager.getConnection("jdbc:sqlite:huonekalut.db");
-            // tee kysely
-            PreparedStatement stmt
-                    = conn.prepareStatement("SELECT nimi FROM Huonekalu");
-            ResultSet tulos = stmt.executeQuery();
-
-            // käsittele kyselyn tulokset
-            while (tulos.next()) {
-                String nimi = tulos.getString("nimi");
-                huonekalut.add(nimi);
-            }
-            // sulje yhteys tietokantaan
-            conn.close();
-
+//            List<String> huonekalut = new ArrayList<>();
+//
+//            // avaa yhteys tietokantaan
+//            Connection conn
+//                    = DriverManager.getConnection("jdbc:sqlite:huonekalut.db");
+//            // tee kysely
+//            PreparedStatement stmt
+//                    = conn.prepareStatement("SELECT nimi FROM Huonekalu");
+//            ResultSet tulos = stmt.executeQuery();
+//
+//            // käsittele kyselyn tulokset
+//            while (tulos.next()) {
+//                String nimi = tulos.getString("nimi");
+//                huonekalut.add(nimi);
+//            }
+//            // sulje yhteys tietokantaan
+//            conn.close();
+//
             HashMap map = new HashMap<>();
 
-            map.put("lista", huonekalut);
+            map.put("moi", "moi");
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
+    }
+    
+    public static Connection getConnection() throws Exception {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (dbUrl != null && dbUrl.length() > 0) {
+            return DriverManager.getConnection(dbUrl);
+        }
+
+        return DriverManager.getConnection("jdbc:sqlite:huonekalut.db");
     }
 
 }
